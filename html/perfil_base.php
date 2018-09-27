83b6237e21220d7ff79a2e829e630df9d375c355
helen2025 <elenagaska@gmail.com>
2018-09-27 14:42:54 -0300

    clase

---
+++ html/perfil_base.php
@@ -0,0 +1,62 @@
+<?php
+include "funciones.php";
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/estilos_v2.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <title>CONCRETA</title>
+  </head>
+  <body>
+    <div class="container_perfil">
+    <?php include("header.php") ?>
+    <section class="seccion_perfil">
+
+     <div class="usuario_basico">
+      <div class="nombre_foto">
+
+        <form class="" action="index.html" method="post">
+         <input class="cambiar_foto"  type="file" name="avatar" value="">
+         <button type="submit" name="button"></button>
+        </form>
+         <?php if (noHayAvatar()): ?>
+         <img class="foto_perfil" src="../img/Perfil_default.jpg" alt="">
+          <?php endif; ?>
+         <p class="txt_centrado"><?php
+         /*traerlo después de la base de datos*/
+         if ($_POST) {
+         echo $_POST["nombre"] . " " . $_POST["apellido"];
+         }
+         else {
+           echo "Nombre Apellido";
+         }
+          ?></p>
+      </div>
+      <div class="rubro_portada">
+        <div class="">
+          <select class="select rubro_seleccionar" name="RUBRO">
+            <option value="null"  selected disabled>Selecciona rubro</option>
+            <option value="alba">Albañeria</option>
+            <option value="gas">Gas</option>
+            <option value="elect">Electricidad</option>
+            <option value="pisorevest">Pisos y Revestimientos</option>
+            <option value="estruct">Estructura</option>
+            <option value="transpor">Trasporte, Carga y Descarga</option>
+          </select>
+        </div>
+      </div>
+    </div>
+
+    </section>
+
+  <footer class="footer_perfil">
+  <?php include("footer.php") ?>
+  </footer>
+
+   </div>
+  </body>
+</html>
