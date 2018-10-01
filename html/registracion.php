34209157eecf202a323760c518bec301cff1709a
Sebastián Rodero <sebastianrodero@MacBook-Pro-de-Sebastian.local>
2018-09-30 23:07:03 -0300

    icono usuario

--- html/registracion.php
+++ html/registracion.php
@@ -33,7 +33,7 @@ $emailDefault = "";
     <meta charset="utf-8">
 
     <title>REGISTRACION</title>
-
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/estilos_v2.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
